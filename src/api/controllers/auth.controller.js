18c78a5b306471d3294f8a4cb5c4ab8472aacfb3
Bahbi Lee <superbahbi@users.noreply.github.com>
2020-03-12 19:33:42 -0700

    Added jwt sign

--- src/api/controllers/auth.controller.js
+++ src/api/controllers/auth.controller.js
@@ -10,7 +10,6 @@ exports.login = async (req, res, next) => {
   User.findOne({ where: { username: req.body.username } })
     .then(user => {
       if (user) {
-        console.log(user);
         if (!user) {
           res.status(400).json("Invalid username or password");
         } else if (!user.validPassword(req.body.password)) {
--- src/api/models/user.js
+++ src/api/models/user.js
@@ -1,6 +1,6 @@
 "use strict";
 const bcrypt = require("bcrypt");
-const jwt = require("jwt-simple");
+const jwt = require("jsonwebtoken");
 const moment = require("moment");
 const { jwtSecret, jwtExpirationInterval } = require("../../config/vars");
 module.exports = (sequelize, DataTypes) => {
@@ -24,15 +24,17 @@ module.exports = (sequelize, DataTypes) => {
   User.prototype.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
   };
-  User.prototype.token = () => {
+  User.prototype.token = function() {
     const playload = {
       exp: moment()
         .add(jwtExpirationInterval, "minutes")
         .unix(),
       iat: moment().unix(),
-      sub: this.id
+      sub: this.id,
+      username: this.username
     };
-    return jwt.encode(playload, jwtSecret);
+    console.log(this.username);
+    return jwt.sign(playload, jwtSecret);
   };
   User.associate = function(models) {
     // associations can be defined here
