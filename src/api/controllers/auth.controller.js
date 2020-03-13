f3e2acebba5657eb04ce68020bcdcb000f799595
Bahbi Lee <superbahbi@users.noreply.github.com>
2020-03-12 19:25:19 -0700

    Added login controller

--- src/api/controllers/auth.controller.js
+++ src/api/controllers/auth.controller.js
@@ -1,7 +1,34 @@
 const User = require("../models").User;
 
 exports.login = async (req, res, next) => {
-  res.json("login");
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
+  User.findOne({ where: { username: req.body.username } })
+    .then(user => {
+      if (user) {
+        console.log(user);
+        if (!user) {
+          res.status(400).json("Invalid username or password");
+        } else if (!user.validPassword(req.body.password)) {
+          res.status(400).json("Invalid username or password");
+        } else {
+          //return token here
+          const token = user.token();
+          res.status(200).json(token);
+        }
+      } else {
+        res.status(400).json("Cant find user");
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while logging in."
+      });
+    });
 };
 exports.register = async (req, res, next) => {
   console.log(req.body);
@@ -24,7 +51,7 @@ exports.register = async (req, res, next) => {
     .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the Tutorial."
+          err.message || "Some error occurred while creating the new user."
       });
     });
 };
--- src/api/models/user.js
+++ src/api/models/user.js
@@ -1,11 +1,39 @@
-'use strict';
+"use strict";
+const bcrypt = require("bcrypt");
+const jwt = require("jwt-simple");
+const moment = require("moment");
+const { jwtSecret, jwtExpirationInterval } = require("../../config/vars");
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    email: DataTypes.STRING,
-    name: DataTypes.STRING
-  }, {});
+  const User = sequelize.define(
+    "User",
+    {
+      username: DataTypes.STRING,
+      password: DataTypes.STRING,
+      email: DataTypes.STRING,
+      name: DataTypes.STRING
+    },
+    {
+      hooks: {
+        beforeCreate: user => {
+          const salt = bcrypt.genSaltSync(10);
+          user.password = bcrypt.hashSync(user.password, salt);
+        }
+      }
+    }
+  );
+  User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  User.prototype.token = () => {
+    const playload = {
+      exp: moment()
+        .add(jwtExpirationInterval, "minutes")
+        .unix(),
+      iat: moment().unix(),
+      sub: this.id
+    };
+    return jwt.encode(playload, jwtSecret);
+  };
   User.associate = function(models) {
     // associations can be defined here
   };
