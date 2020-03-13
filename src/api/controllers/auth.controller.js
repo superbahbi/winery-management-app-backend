3958d96d058a3d9d3a8d0501bde0ee4bf7adc9aa
Bahbi Lee <superbahbi@users.noreply.github.com>
2020-03-12 18:50:40 -0700

    Added register controller

--- src/api/controllers/auth.controller.js
+++ src/api/controllers/auth.controller.js
@@ -1,8 +1,32 @@
+const User = require("../models").User;
+
 exports.login = async (req, res, next) => {
   res.json("login");
 };
 exports.register = async (req, res, next) => {
-  res.json("register");
+  console.log(req.body);
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
+  const newUser = {
+    username: req.body.username,
+    password: req.body.password,
+    name: req.body.name,
+    email: req.body.email
+  };
+  User.create(newUser)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Tutorial."
+      });
+    });
 };
 exports.refresh = async (req, res, next) => {
   res.json("refresh");
