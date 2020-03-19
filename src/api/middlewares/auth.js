92913a225a95290e506dfa128f134227755a250c
Bahbi Lee <superbahbi@users.noreply.github.com>
2020-03-18 19:24:58 -0700

    Added passport jwt api authentication

--- src/api/middlewares/auth.js
+++ src/api/middlewares/auth.js
@@ -1,1 +1,19 @@
-exports.authorize = () => true;
+const passport = require("passport");
+exports.authorize = () => (req, res, next) =>
+  passport.authenticate("jwt", { session: false }, handleJWT(req, res, next))(
+    req,
+    res,
+    next
+  );
+const handleJWT = (req, res, next) => async (err, user, info) => {
+  const apiError = new {
+    message: error ? error.message : "Unauthorized",
+    status: httpStatus.UNAUTHORIZED,
+    stack: error ? error.stack : undefined
+  }();
+  if (err || info) {
+    next(apiError);
+  }
+  console.log(info);
+  return next();
+};
--- src/config/passport.js
+++ src/config/passport.js
@@ -2,13 +2,14 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const BearerStrategy = require("passport-http-bearer");
 const { ExtractJwt } = require("passport-jwt");
 const { jwtSecret } = require("./vars");
+const User = require("../api/models").User;
 
 var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = jwtSecret;
 const jwt = async (payload, done) => {
   try {
-    const user = await User.findById(payload.sub);
+    const user = await User.findByPk(payload.sub);
     if (user) return done(null, user);
     return done(null, false);
   } catch (error) {
