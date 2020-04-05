02a95006632f5c915b2e7b530f042f1be30d396f
Bahbi Lee <superbahbi@users.noreply.github.com>
2020-04-05 16:11:11 -0700

    Deleted unused routes

--- src/api/routes/v1/batch.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const controller = require("../../controllers/batch.controller");
-const router = express.Router();
-router.route("/").get(controller.all);
-router.route("/add").post(controller.add);
-router.route("/edit").put(controller.edit);
-router.route("/delete").delete(controller.delete);
-
-module.exports = router;
--- src/api/routes/v1/workorder.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const controller = require("../../controllers/workorder.controller");
-const { authorize } = require("../../middlewares/auth");
-const router = express.Router();
-router.route("/").get(controller.all);
-router.route("/add").post(controller.add);
-
-module.exports = router;
